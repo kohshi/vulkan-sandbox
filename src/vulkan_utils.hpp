246af6f47d1338af19e2d2b21b7bde11b086a6b9
Masashi.Eshima <Masashi.Eshima@sony.com>
2025-04-15 21:06:55 +0900

    Fixed error message format.

--- src/vulkan_utils.hpp
+++ src/vulkan_utils.hpp
@@ -3,7 +3,7 @@
 
 #define CHK(result) \
   if (result != VK_SUCCESS) { \
-    std::cerr << "Vulkan API error: "<< result << __LINE__ << "at " << __FILE__ << std::endl; \
+    std::cerr << "Vulkan API error: "<< result << " l" <<  __LINE__ << " at " << __FILE__ << std::endl; \
     exit(-1); \
   }
 
