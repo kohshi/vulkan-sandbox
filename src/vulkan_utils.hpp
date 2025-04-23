1f2525d563dd2489abab2e5343fbab334d75468a
Masashi.Eshima <Masashi.Eshima@sony.com>
2025-04-23 10:22:50 +0900

    Inserted string_VkResult.

--- src/vulkan_utils.hpp
+++ src/vulkan_utils.hpp
@@ -1,9 +1,10 @@
 #pragma once
+#include <vulkan/vk_enum_string_helper.h>
 #include <iostream>
 
 #define CHK(result) \
   if (result != VK_SUCCESS) { \
-    std::cerr << "Vulkan API error: "<< result << " " <<  __LINE__ << " at " << __FILE__ << std::endl; \
+    std::cerr << "Vulkan API error: "<< string_VkResult(result) << " " <<  __LINE__ << " at " << __FILE__ << std::endl; \
     exit(-1); \
   }
 
